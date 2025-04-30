dc1f23e60bd634084d5982ea690c680ec7a9e2b3
Raquel Rosa <raquelrosa14@gmail.com>
2025-04-30 11:46:57 -0300

    Commit 9: Replacing conditional with polymorphism - part 2.

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ class Price:
     def get_frequent_renter_points(self, days_rented: int) -> int:
         pass
 
-class RegulaPrice(Price):
+class RegularPrice(Price):
     pass
 
 class NewReleasePrice(Price):
@@ -16,35 +16,26 @@ class ChildrenPrice(Price):
     pass
 
 class Book:
-
     REGULAR: int = 0
     NEW_RELEASE: int = 1
     CHILDREN: int = 2
 
     def __init__(self, title: str, price_code: int):
         self.title = title
-        self.price_code = price_code
+        self.price = self.create_price(price_code)
 
-    def get_charge(self, days_rented: int) -> float:
-        amount = 0
+    def create_price(self, price_code: int):
+        if price_code == Book.NEW_RELEASE:
+            return NewReleasePrice()
+        elif price_code == Book.CHILDREN:
+            return ChildrenPrice()
+        return RegularPrice()
 
-        if self.price_code == Book.REGULAR:
-            amount += 2
-            if days_rented > 2:
-                amount += (days_rented - 2) * 1.5
-        elif self.price_code == Book.NEW_RELEASE:
-            amount += days_rented * 3
-        elif self.price_code == Book.CHILDREN:
-            amount += 1.5
-            if days_rented > 3:
-                amount += (days_rented - 3) * 1.5
-        return amount
+    def get_charge(self, days_rented: int):
+        return self.price.get_charge(days_rented)
 
     def get_frequent_renter_points(self, days_rented: int):
-        points = 1
-        if self.price_code == Book.NEW_RELEASE and days_rented > 1:
-            points += 1
-        return points
+        return self.price.get_frequent_renter_points(days_rented)
 
 class Rental:
     def __init__(self, book: Book, days_rented: int):
